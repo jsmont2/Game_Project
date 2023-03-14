372947c2835d5f1928723d9dc4a77bd607d24714
jsmont2 <jsaaamonteon@gmail.com>
2023-03-14 15:55:56 -0700

    changed the camera positions and zoomed out player cam

--- Castlepoint/Assets/Scripts/CameraMovement.cs
+++ Castlepoint/Assets/Scripts/CameraMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     public Transform target; //The obj you want the camera to follow
-    public  float smoothing; // how quickly the cam moves twords the target.
+    public  float smoothing; // how quickly the cam moves towards the target.
     public Vector2 maxPosition;
     public Vector2 minPosition;
     // Start is called before the first frame update
@@ -17,7 +17,7 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        if(transform.position != target.position)//this finds the distance between itself and the target so it canmove at a certain percentage.
+        if(transform.position != target.position)//this finds the distance between itself and the target so it can move at a certain percentage.
         {
             Vector3 targetPosition = new Vector3(target.position.x,target.position.y, transform.position.z);
 
