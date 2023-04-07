d6be38b3ab8fd4cafc6ea29f14d7d1547b7aea07
OaP7 <paezomar718@gmail.com>
2023-04-06 19:36:50 -0700

    part 16 and 17
    I add a basic AI and knockback on attacks.

--- Castlepoint/Assets/Scripts/pinkslime.cs
+++ Castlepoint/Assets/Scripts/pinkslime.cs
@@ -2,21 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class pinkslime : MonoBehaviour
+public class pinkslime : Enemy // inherates everything from enemy script including mono behavior
 {
     private Animator anim;
-
+    public Transform target;
+    public float chaseRadius;
+    public float attackRadius;
+    public Transform homePosition;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        target = GameObject.FindWithTag("Player").transform;
+        
     }
 
     // Update is called once per frame
     void Update()
+    
     {
-        
+        CheckDistance();
+    }
+    void CheckDistance()
+    {
+        if(Vector3.Distance(target.position, transform.position) <= chaseRadius && Vector3.Distance(target.position, transform.position) > attackRadius)// this check the distance between enemy and player to dictate what the enemy will do.
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+        }
     }
 
     public void Hit()
