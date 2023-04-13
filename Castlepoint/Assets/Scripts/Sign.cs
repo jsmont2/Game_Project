c4f32d79dbd866f0a917c8da7c90d75ce17d098c
jsmont2 <jsaaamonteon@gmail.com>
2023-04-12 21:36:55 -0700

    changed sign interaction button to the key E

--- Castlepoint/Assets/Scripts/Sign.cs
+++ Castlepoint/Assets/Scripts/Sign.cs
@@ -21,7 +21,7 @@ public class Sign : MonoBehaviour
 
     // Update is called once per frame
     void Update(){
-        if (Input.GetKeyDown(KeyCode.Space) && playerInRange)
+        if (Input.GetKeyDown(KeyCode.E) && playerInRange)
         {
             if (dialogBox.activeInHierarchy)
             {
